d4284502cbccddc0f3cc78d0c3a7f6e9151215d4
Sky Yin <sky.yin@gmail.com>
2021-03-30 00:58:27 -0700

    simplier and the fastest version of move zeroes

--- array/move_zeroes.go
+++ array/move_zeroes.go
@@ -10,28 +10,20 @@ func moveZeroes(nums []int) {
 	for ; j < len(nums); j++ {
 		// seek until finding a non-zero
 		if nums[j] != 0 {
-			// shift items after 0 backward
-			for k := 0; k < len(nums)-j; k++ {
-				nums[i+k] = nums[j+k]
-			}
+			// swap
+			temp := nums[i]
+			nums[i] = nums[j]
+			nums[j] = temp
 
-			// fill zeros at the end
-			for l := 0; l < j - i; l++ {
-				nums[len(nums) -1 - l] = 0
-			}
-
-			// reset j to check items after pointer i
-			j = i
 			i += 1
 		}
-
 	}
 }
 
 func main() {
-	// list := []int{0, 1, 0, 3, 12}
+	list := []int{0, 1, 0, 3, 12}
 	// list := []int{0, 0, 1, 0, 0, 13, 0, 2}
-	list := []int{1}
+	// list := []int{1}
 
 	moveZeroes(list)
 	fmt.Println(list)
