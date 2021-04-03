f64621f27b8a97fe6cffaa64041119c8420c8fed
Sky Yin <sky.yin@gmail.com>
2021-04-03 04:50:56 -0700

    solution to reverse string

---
+++ strings/reverse_string.go
@@ -0,0 +1,20 @@
+// Move zeros
+
+package main
+
+import "fmt"
+
+func reverseString(s []byte) {
+	for i := 0; i < len(s)/2; i++ {
+		temp := s[i]
+		s[i] = s[len(s)-i-1]
+		s[len(s)-i-1] = temp
+	}
+}
+
+func main() {
+	str := []byte{'H', 'a', 'n', 'n', 'a', 'h'}
+
+	reverseString(str)
+	fmt.Printf(string(str))
+}
