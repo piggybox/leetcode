55db8d0b6ea13e0503d5f3ec325f224c773132e1
Sky Yin <sky.yin@gmail.com>
2021-04-22 13:55:35 -0700

    solution to 3 consecutive number

---
+++ database/consecutive_numbers.sql
@@ -0,0 +1,11 @@
+SELECT
+    DISTINCT t1.num ConsecutiveNums
+FROM
+    LOGS t1
+    JOIN LOGS t2
+    JOIN LOGS t3
+WHERE
+    t1.num = t2.num
+    AND t2.num = t3.num
+    AND t2.id - t1.id = 1
+    AND t3.id - t2.id = 1
