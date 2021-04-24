733632db85fb9f515c8319ce8367e948e8f3ff8e
Sky Yin <sky.yin@gmail.com>
2021-04-24 15:39:42 -0700

    simple solution of palindrome linked list by using stack

---
+++ linked_list/palindrome_linked_list.py
@@ -0,0 +1,29 @@
+# Palindrome Linked List
+# easy
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        pointer = head
+        stack = []
+
+        # push to stack
+        while pointer != None:
+            stack.append(pointer.val)
+            pointer = pointer.next
+
+        # check the list against the stack
+        pointer = head
+        while pointer != None:
+            if pointer.val == stack.pop():
+                pointer = pointer.next
+                continue
+            else:
+                return False
+
+        return True
