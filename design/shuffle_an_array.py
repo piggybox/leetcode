c1984859509e09005bba61a72d41fbf3c61e21c3
Sky Yin <sky.yin@gmail.com>
2021-04-24 03:07:50 -0700

    solution to shuffle array

---
+++ design/shuffle_an_array.py
@@ -0,0 +1,29 @@
+# shuffle an array
+# medium
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.list = nums
+        
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.list
+        
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        import random
+        return random.sample(self.list, len(self.list))
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
