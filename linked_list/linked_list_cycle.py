f2dc34d3ed2b50ef5aad56ba88222728c93b188d
Sky Yin <sky.yin@gmail.com>
2021-04-24 02:16:42 -0700

    solution to inked list cycle

---
+++ linked_list/linked_list_cycle.py
@@ -0,0 +1,26 @@
+# Linked List Cycle
+# easy
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        p1 = head  # slow pointer
+        p2 = head  # faster pointer
+
+        cycle = False 
+
+        while p1 != None and p2 != None and p2.next != None :
+            p2 = p2.next.next
+            p1 = p1.next 
+
+            if p1 == p2:
+                cycle = True
+                break
+
+
+        return cycle
