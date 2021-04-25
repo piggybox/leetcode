cdbc79ebe0457b88f4648050748813e134f9b9ca
Sky Yin <sky.yin@gmail.com>
2021-04-24 20:25:54 -0700

    solution to sum of digits in base

---
+++ math/sum_of_digits_in_base_k.go
@@ -0,0 +1,28 @@
+// Sum of Digits in Base K
+
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+)
+
+func sumBase(n int, k int) int {
+	converted := big.NewInt(int64(n)).Text(k)
+
+	result := 0
+	for _, char := range converted {
+		num, _ := strconv.Atoi(string(char))
+		result += num
+	}
+
+	return result
+}
+
+func main() {
+	n := 10
+	k := 10
+	result := sumBase(n, k)
+	fmt.Println(result)
+}
