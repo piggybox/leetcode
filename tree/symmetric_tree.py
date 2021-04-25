9608d1d2d7c4ab6f80788f62bd4e6ff0cac843fe
Sky Yin <sky.yin@gmail.com>
2021-04-25 00:39:49 -0700

    solution to symmetric tree

---
+++ tree/symmetric_tree.py
@@ -0,0 +1,24 @@
+# easy
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def isMissor(t1: TreeNode, t2: TreeNode) -> bool:
+            if t1 == None and t2 == None:
+                return True
+
+            if t1 == None or t2 == None:
+                return False
+
+            return t1.val == t2.val and isMissor(
+                t1.right, t2.left) and isMissor(t1.left, t2.right)
+
+        return isMissor(root, root)
