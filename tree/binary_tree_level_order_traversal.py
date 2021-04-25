f2697156a1b24a06b38dbd6fb3583ef69701ad1f
Sky Yin <sky.yin@gmail.com>
2021-04-25 01:28:00 -0700

    solution to binary tree order traversal

---
+++ tree/binary_tree_level_order_traversal.py
@@ -0,0 +1,33 @@
+# medium
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        hash = {}
+
+        def traversal(node: TreeNode, level):
+            if node != None:
+                if level in hash:
+                    hash[level].append(node.val)
+                else:
+                    hash[level] = [node.val]
+                traversal(node.left, level+1)
+                traversal(node.right, level+1)
+
+        traversal(root, 0)
+
+        # convert hash
+        result = []
+        for k in hash:
+            result.append(hash[k])
+
+        return result
+
+
+
