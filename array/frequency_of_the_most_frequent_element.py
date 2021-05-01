0c421ab6ab0655dc0e40aaa6164f2cbdb9c8fa91
Sky Yin <sky.yin@gmail.com>
2021-05-01 13:00:15 -0700

    staged change

---
+++ array/frequency_of_the_most_frequent_element.py
@@ -0,0 +1,25 @@
+# Frequency of the Most Frequent Element
+# medium
+
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        max_count = 0
+
+        for i in nums :
+            # find difference
+            temp = [x-i for x in nums if x-i >= 0]
+            
+            # search distribution of k among difference
+            # dynamic programming problem?
+            target = k 
+            count = 0
+            for i in temp:
+                if k - i > 0:
+                    k -= i
+                    count += 1
+
+            if count > max_count:
+                max_count += count 
+
+        return max_count
+
---
+++ design/min_stack.py
@@ -0,0 +1,27 @@
+class MinStack:
+    
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        
+
+    def push(self, val: int) -> None:
+        pass
+
+    def pop(self) -> None:
+        pass
+
+    def top(self) -> int:
+        pass
+
+    def getMin(self) -> int:
+        pass
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
