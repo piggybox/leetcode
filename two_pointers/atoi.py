e870e35f6065ebd7e2b7099e19a9084984168bcb
Sky Yin <sky.yin@gmail.com>
2024-01-01 20:16:21 -0800

    better version with single index

--- two_pointers/atoi.py
+++ two_pointers/atoi.py
@@ -1,31 +1,27 @@
 def myAtoi(s):
-    s = s.strip()  # remove white space
-
     sign = 1
-    i, j = 0, 0  # pointers
-    i_flag = True
+    i = 0  # pointers
     result = 0
-
     num_chars = list(map(lambda x: str(x), range(0, 10)))
 
-    while i_flag:
+    # skip white space
+    while i < len(s) and s[i] == " ":
+        i += 1
+    # process +/- symbols
+    while i < len(s) and (s[i] == "+" or s[i] == "-"):
         if s[i] == "-":
             sign = -1
-            i += 1
-        elif s[i] == "+":
-            i += 1
-        elif s[i] not in num_chars:  # non-numeric
-            return 0
+        i += 1
+
+    while i < len(s):
+        if s[i] not in num_chars:  # non-numeric
+            break
         elif s[i] in num_chars:
-            i_flag = False  # stop moving pointer i
-            result += int(s[i])
-            j = i + 1
-            while j < len(s) and s[j] in num_chars:
-                result = result * 10 + int(s[j])
-                j += 1
+            result = result * 10 + int(s[i])
+            i += 1
 
     if result * sign > 2**31 - 1:
         return 2**31 - 1
-    elif result * sign < -2**31:
-        return -2**31  
+    elif result * sign < -(2**31):
+        return -(2**31)
     return result * sign
