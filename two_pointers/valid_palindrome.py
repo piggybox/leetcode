c24a307d99066c73653afc2d51281faceeab9981
Sky Yin <sky.yin@gmail.com>
2024-01-15 14:42:30 -0800

    valid palindrome

---
+++ two_pointers/valid_palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    # @param {string} s
+    # @return {boolean}
+
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+
+        return True
