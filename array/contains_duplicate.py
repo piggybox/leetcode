028322e79901948ec999a2069712dfaa88182efc
Sky Yin <sky.yin@gmail.com>
2025-01-11 12:42:59 -0800

    Fast implementation for 217

---
+++ array/contains_duplicate.py
@@ -0,0 +1,7 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # super fast implementation using internal set function
+        if len(set(nums)) == len(nums):
+            return False 
+        else:
+            return True
