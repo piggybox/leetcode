03b4599996351f042409653255a2924b32f2873a
Sky Yin <sky.yin@gmail.com>
2025-01-11 15:10:36 -0800

    Solution for 448

---
+++ array/find_all_missing_numbers.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        set_nums = set(nums)
+        result = []
+
+        for i in range(1, len(nums) + 1):
+            if i not in set_nums:
+                result.append(i)
+
+        return result
