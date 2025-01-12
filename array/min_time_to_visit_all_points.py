dee6cb2022100e6e11f6478866789f2a3a1566e1
Sky Yin <sky.yin@gmail.com>
2025-01-11 20:53:07 -0800

    Solution to 1266

---
+++ array/min_time_to_visit_all_points.py
@@ -0,0 +1,11 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        # math trick problem, not traveling sales man problem
+        res = 0
+        n = len(points)
+        for i in range(n - 1):
+            x1, y1 = points[i][0], points[i][1]
+            x2, y2 = points[i + 1][0], points[i + 1][1]
+            res += max(abs(y2 - y1), abs(x2 - x1))
+
+        return res
