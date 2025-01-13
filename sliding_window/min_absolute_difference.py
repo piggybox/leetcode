57ea14fa7733d9f453527046925f74a63176c518
Sky Yin <sky.yin@gmail.com>
2025-01-13 12:46:47 -0800

    Rewrote sorting

--- sliding_window/min_absolute_difference.py
+++ sliding_window/min_absolute_difference.py
@@ -2,18 +2,18 @@ from typing import List
 
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        sorted_arr = sorted(arr)
+        arr.sort()
         result = []
-        min_abs_diff = abs(sorted_arr[0] - sorted_arr[1])
+        min_abs_diff = abs(arr[0] - arr[1])
 
-        for i in range(len(sorted_arr) - 1):
-            abs_diff = abs(sorted_arr[i] - sorted_arr[i + 1])
+        for i in range(len(arr) - 1):
+            abs_diff = abs(arr[i] - arr[i + 1])
             
             if abs_diff < min_abs_diff:
                 # reset result
-                result = [[sorted_arr[i], sorted_arr[i + 1]]]
+                result = [[arr[i], arr[i + 1]]]
                 min_abs_diff = abs_diff
             elif abs_diff == min_abs_diff:
-                result.append([sorted_arr[i], sorted_arr[i + 1]])
+                result.append([arr[i], arr[i + 1]])
             
         return result
