991ff58f559208ee6462108e6d9eefbd5b1edcd1
Sky Yin <sky.yin@gmail.com>
2025-01-13 14:39:17 -0800

    Very smart sliding window solution for 209

---
+++ sliding_window/min_size_subarray_sum.py
@@ -0,0 +1,25 @@
+import sys 
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        res = len(nums)
+        total = 0
+
+        if sum(nums) < target:
+            return 0
+
+        # dynamic sliding window
+        left  = 0
+        
+        # it's important to check through all possible right
+        for right in range(len(nums)): 
+            # accumulate right until total is bigger than target
+            total += nums[right]
+            
+            # then try to shrink the left side
+            while total >= target:
+                res = min(res, right - left +1)
+                total -= nums[left]
+                left += 1
+
+        return res
