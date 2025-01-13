3e4193e2dd6f14cfb297dd82dadf27c155177c98
Sky Yin <sky.yin@gmail.com>
2025-01-13 02:41:48 -0800

    Reformat

--- two_pointers/square_of_sorted_array.py
+++ two_pointers/square_of_sorted_array.py
@@ -6,13 +6,13 @@ class Solution:
         # scan from two ends
         result = []
         left, right = 0, len(nums) - 1
-        
+
         while left <= right:
             if abs(nums[left]) > abs(nums[right]):
-                result.append(nums[left]**2)
+                result.append(nums[left] ** 2)
                 left += 1
             else:
-                result.append(nums[right]**2)
+                result.append(nums[right] ** 2)
                 right -= 1
-                
-        return result[::-1] # also can use deque and appendleft
+
+        return result[::-1]  # also can use deque and appendleft
