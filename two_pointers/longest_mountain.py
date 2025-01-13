f1bd12c2f067451d82cb802ce9b57cefae82e7b5
Sky Yin <sky.yin@gmail.com>
2025-01-13 10:55:02 -0800

    Optimized version on 845

--- two_pointers/longest_mountain.py
+++ two_pointers/longest_mountain.py
@@ -5,28 +5,21 @@ class Solution:
     def longestMountain(self, arr: List[int]) -> int:
         max_mountain = 0
         for i in range(1, len(arr)-1):
-            # scan mid point from 1 to n-1
-            
-            # for each i scan on both sides
-            left_size, right_size = 0, 0
-            left, right = i - 1, i + 1
-            
-            while left >= 0:
-                if arr[left] < arr[left+1]:
-                    left_size += 1
-                    left -= 1
-                else: 
-                    break
-        
-            while right < len(arr):
-                if arr[right] < arr[right-1]:
-                    right_size += 1
-                    right += 1
-                else:
-                    break
+            # check minimal condition for a mountain
+            if arr[i-1] < arr[i] > arr[i+1]:
+                # for each i scan on both sides
+                left, right = i - 1, i + 1
                 
-            if left_size > 0 and right_size > 0:
-                # there's a mountain
-                max_mountain = max(max_mountain, left_size + right_size + 1)
+                while left >= 0 and arr[left] < arr[left+1]:
+                    left -= 1
+
+            
+                while right <= len(arr) and arr[right] < arr[right-1]:
+                    right += 1
+
+                # -2 as the final step of while loop does one extra movement
+                # alternatively, we can initialize left and right with i on L11
+                max_mountain = max(max_mountain, right - left + 1 - 2) 
+                #print(i, max_mountain)
         
         return max_mountain
