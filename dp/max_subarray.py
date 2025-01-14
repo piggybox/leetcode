1df303e4542f7f4d546958160db8dd48b5dd80bf
Sky Yin <sky.yin@gmail.com>
2025-01-13 20:33:51 -0800

    Add comment

--- dp/max_subarray.py
+++ dp/max_subarray.py
@@ -7,6 +7,8 @@ class Solution:
         dp = [0] * n
 
         for i in range(n):
+            # If the previous sum is negative, we just start it over
+            # If not, we can add the current number
             dp[i] = max(dp[i - 1] + nums[i], nums[i])
 
         return max(dp)
