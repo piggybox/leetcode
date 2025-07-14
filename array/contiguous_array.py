9345e7206a0193afa0210307831e2537708a9424
Sky Yin <sky.yin@gmail.com>
2025-07-14 02:15:13 -0700

    Refactor comments for clarity in findMaxLength method

--- array/contiguous_array.py
+++ array/contiguous_array.py
@@ -5,9 +5,11 @@ class Solution:
         count = 0
 
         for i, num in enumerate(nums):
-            count += 1 if num == 1 else -1 # Increment for 1, decrement for 0!
+            count += 1 if num == 1 else -1 # prefix sum logic
             
             if count in count_map:
+                # If the prefix sum at two indices is the same, 
+                # it means that the number of 0s and 1s between those two indices is the same!
                 max_length = max(max_length, i - count_map[count])
             else:
                 count_map[count] = i
